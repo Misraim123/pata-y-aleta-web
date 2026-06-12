37f9b08938a0d28bb91300622a523a9fad2fc8e6
Misraim123 <ian_raim10.1@hotmail.com>
2026-06-11 18:32:56 -0600

    test env vars

--- admin-panel/api/send-lead.js
+++ admin-panel/api/send-lead.js
@@ -1,15 +1,8 @@
-import { Resend } from 'resend'
-
 export default async function handler(req, res) {
 
   return res.status(200).json({
 
-    apiKeyExiste: !!process.env.RESEND_API_KEY,
-
-    apiKeyPrimeros5:
-      process.env.RESEND_API_KEY
-      ? process.env.RESEND_API_KEY.substring(0,5)
-      : null
+    todas: Object.keys(process.env)
 
   })
 
